f506bae018fa166fe6c85623a66c4bda6a259e04
NguyenPhuDuc307 <95112332+NguyenPhuDuc307@users.noreply.github.com>
2022-01-04 14:44:56 +0700

    aaaaa

--- VANPHONGPHAM/frmDelete.cs
+++ VANPHONGPHAM/frmDelete.cs
@@ -24,6 +24,7 @@ namespace VANPHONGPHAM
         //dhyegdfcyegfydce
         // ciuvhrijdso
         //fdudgydgcye
+        //uhudcgeudh
         private void frmDelete_Load(object sender, EventArgs e)
         {
             _cthd = new HOADONMATHANG();
